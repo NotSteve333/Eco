8e71b97292b881eb9c42461e3b7969e36d07eb0a
NotSteve333 <alexbro0106@gmail.com>
2025-08-23 15:27:52 -0500

    just makin shore

--- eco/controls_buffer.gd
+++ eco/controls_buffer.gd
@@ -15,20 +15,22 @@ func _ready() -> void:
 	for i in range(buffer_size):
 		buffer.append("none")
 
+func is_pressed(action: String) -> bool:
+	return Input.is_action_pressed(action)
+
 # Direction is often used by Player with only the x value, but why not track everything?
 func _process(_delta: float) -> void:
 	var dir_x = Input.get_axis("left", "right")
 	var dir_y = Input.get_axis("up", "down")
 	direction = Vector2(dir_x, dir_y)
+	
+	for action in buffer_actions:
+		if Input.is_action_pressed(action) and !action in buffer:
+			buffer[buffer_size - 1] = action
+			advance_buffer()
 
 func _physics_process(_delta: float) -> void:
 	advance_buffer()
-
-func _input(event) -> void:
-	for action in buffer_actions:
-		if event.is_action_pressed(action) and !action in buffer:
-			buffer[buffer_size - 1] = action
-			advance_buffer()
 	
 func advance_buffer() -> void:
 	for i in range(buffer_size - 1):
