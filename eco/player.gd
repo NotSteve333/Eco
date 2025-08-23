de9f2bfd390ce14aeb1d77820c4aadc4f5ce336b
NotSteve333 <alexbro0106@gmail.com>
2025-08-23 13:55:05 -0500

    controls

--- eco/player.gd
+++ eco/player.gd
@@ -3,18 +3,24 @@ class_name Player
 
 @onready var controls = $Controls
 @onready var camera = %Camera2D
+@onready var animation = $AnimatedSprite2D
 
 @export var speed = 300.0
 
 var dir_x: float
 
 func _physics_process(delta: float) -> void:
-	# dir_x = controls.get_direction().x
-	
-	# velocity.x = move_toward(velocity.x, dir_x * speed, speed / 3.0)
-	velocity = velocity.move_toward(controls.get_direction().normalized() * speed, speed / 3.0)
-	
-	#if !is_on_floor():
-		#velocity += get_gravity()
-		
+	dir_x = controls.get_direction().x
+	velocity.x = move_toward(velocity.x, dir_x * speed, speed / 3.0)
+
+	if !is_on_floor():
+		velocity += get_gravity()
+
 	move_and_slide()
+
+func _process(_delta: float) -> void:
+	if abs(dir_x) > 0.01:
+		animation.play("walk")
+		animation.flip_v = dir_x < 0
+	else:
+		animation.play("idle")
